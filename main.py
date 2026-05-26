722da44fdf436afcf0c5f92a84d82a3558d56455
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-26 14:08:21 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class metricsSonnectWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = metricsSonnectWorker()
+    print("Worker engine initialized.")
